26bd5697cb9173a7d85b4e3fba808079389e5ea3
606780 <carlosgarcia@C02YX0ZQLVDT.fios-router.home>
2019-09-02 20:58:40 -0400

    converted to gradle

---
+++ eclipse-workspace/conventionAPI/build/resources/main/data.sql
@@ -0,0 +1,11 @@
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('joe', 'joe@email.com', 'pass');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('bob', 'bob@email.com', 'pass');
+insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('foo', 'foo@email.com', 'pass');
+
+insert into EVENTS (CUSTOMER_ID, NAME, DATE_OF) VALUES (3,'Free Pizza', '2008-11-11');
+insert into EVENTS (CUSTOMER_ID, NAME, DATE_OF) VALUES (2,'Free Cheese', '2008-11-11');
+insert into EVENTS (CUSTOMER_ID, NAME, DATE_OF) VALUES (1,'Free Free', '2008-11-11');
+
+insert into REGISTRATIONS (CUSTOMER_ID, EVENT_ID) VALUES (1,2);
+insert into REGISTRATIONS (CUSTOMER_ID, EVENT_ID) VALUES (3,2);
+insert into REGISTRATIONS (CUSTOMER_ID, EVENT_ID) VALUES (2,2);
